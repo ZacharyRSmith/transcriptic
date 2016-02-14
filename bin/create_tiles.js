7a6ffd84a4a0e6d549dafbdc2afc47dcd23762a6
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-02-14 11:56:24 -0800

    (feat) Implement #findMaxZoomLevel

--- bin/create_tiles.js
+++ bin/create_tiles.js
@@ -1,5 +1,3 @@
-
-
 var fs = require('fs');
 
 var gm = require('gm');
@@ -7,46 +5,50 @@ var gm = require('gm');
 // Promise.promisifyAll(require('gm'));
 
 
-var filePath = process.argv[2];
-
-function removeLastExtension (filePath) {
-  return filePath.split('.').slice(0, -1).join('.');
+function appendCopy (filePath) {
+  var ary = filePath.split('.');
+  ary[ary.length - 2] += '_copy';
+  return ary.join('.');
 }
 
-function topLeftQuarter (filePath) {
-  gm(filePath)
-      .filesize(function (err, fileSize) {
-        console.log("BEFORE", fileSize);
+// TODO Find mathematical way to guesstimate max zoom level
+function findMaxZoomLevel (filePath) {
+  // Took 80s for Heroku to serve a 26.6MB pic
+  // 26.6MB / (80 * 4) = 83KBps
+  // Goal: <100KB per tile
+  var GOAL_SIZE = 100000;
 
-        this.size(function (err, size) {
-          this.resize(size.width * 2)
+  // TODO refactor to recurse on cropped image, not original image, to increase speed
+  function tryMaxZoomLevel (filePath, zoomLevel) {
+    var readStream = fs.createReadStream(filePath);
+    var sizeReductionFactor = Math.pow(2, zoomLevel);
+    var writeStream = fs.createWriteStream(appendCopy(filePath));
+
+    gm(readStream)
+        .size({ bufferStream: true }, function (err, size) {
+          if (err) { return console.error(err); }
+
+          this.crop(size.width / sizeReductionFactor,
+                    size.height / sizeReductionFactor,
+                    0,
+                    0)
               .stream(function (err, stdout, stderr) {
-                var writeStream = fs.createWriteStream(removeLastExtension(filePath) + '_copy.jpg');
                 writeStream.on('close', function () {
-                  console.log("INSIDE 2", writeStream);
+                  if (writeStream.bytesWritten > GOAL_SIZE) {
+                    return tryMaxZoomLevel(filePath, zoomLevel + 1);
+                  } else {
+                    // genTileSets(filePath, maxZoomLevel);
+                    return console.log(zoomLevel);
+                  }
                 });
-                stdout.pipe(writeStream)
+                stdout.pipe(writeStream);
               });
         });
-      });
+  }
+  tryMaxZoomLevel(filePath, 0);
 }
 
-// function findMaxZoomLevel (imgSize) {
-//   // Took 80s for Heroku to serve a 26.6MB pic
-//   // 26.6MB / (80 * 4) = 83KBps
-//   // Goal: <100KB per tile
-//   var GOAL_SIZE = 100000;
 
-//   var maxZoomLevel = 0;
-//   var smallestUncompressedTileSize = imgSize;
-//   var numTilesAtMaxZoomLevel = 1;
-//   while (smallestUncompressedTileSize > GOAL_SIZE) {
-//     numTilesAtMaxZoomLevel *= 4;
-//     smallestUncompressedTileSize /= 4;
-//     maxZoomLevel += 1;
-//   }
-//   console.log('maxZoomLevel:', maxZoomLevel);
-// }
+var filePath = process.argv[2];
 
-// findMaxZoomLevel(picSize);
-topLeftQuarter(filePath);
+findMaxZoomLevel(filePath);
