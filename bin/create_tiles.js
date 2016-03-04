d5e4e9f2d52ac028f806a698238d6d31e8154955
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-03-04 10:22:01 -0800

    (doc) Add TODOs to tiler.js and create_tiles.js

--- bin/create_tiles.js
+++ bin/create_tiles.js
@@ -6,6 +6,7 @@ var tiler = new Tiler(filePath);
 
 tiler.setOriginalImageSize()
     .bind(tiler)
+    // TODO ? move these setters to Tiler constructor function ? 
     .then(tiler.setMaxZoomLevel)
     .then(tiler.setCompressionLevels)
     .then(tiler.genTileSets)
